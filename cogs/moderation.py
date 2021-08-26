6d3a1c61fb8708662b85cf1540e3119ef01f5f7b
SergSel2006 <seregasel44@gmail.com>
2021-08-26 08:29:25 +0700

    rebased cog

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -14,6 +14,32 @@ import yaml
 from discord.ext import commands
 
 
+def load_server_language(message):
+    config = find_server_config(message)
+    language = load_language(config["language"])
+    return language
+
+
+def load_language(lang):
+    with open(
+            pathlib.Path("data", "languages", f"{lang}.yml"), "r",
+            encoding="utf8"
+            ) as lang:
+        lang = yaml.load(lang, Loader=Loader)
+        return lang
+
+
+def find_server_config(message):
+    with open(
+            pathlib.Path(
+                "data", "servers_config", str(message.guild.id),
+                "config.yml"
+                ), "r", encoding="utf8"
+            ) as config:
+        config = yaml.load(config, Loader=Loader)
+        return config
+
+
 def can_manage_channels():
     async def predicate(ctx):
         perms = ctx.author.top_role.permissions
@@ -23,42 +49,33 @@ def can_manage_channels():
 
 
 class ModCog(commands.Cog):
-    """Команды для модераторов сервера. Также имеет модлог"""
-    
     def __init__(self, bot, cwd: pathlib.Path):
         self.bot = bot
         self.cwd = cwd
     
-    @staticmethod
-    def find_server_config(message):
-        return pathlib.Path(
-            "data", "servers_config", str(message.guild.id),
-            "config.yml"
-            )
-    
     @commands.Cog.listener()
     async def on_message_delete(self, msg):
-        with open(self.find_server_config(msg), 'r') as raw_config:
-            config = yaml.load(raw_config, Loader)
-            if config["modlog"]["enabled"]:
-                ch = self.bot.get_channel(config["modlog"]["channel"])
-                await ch.send(
-                    f"Пользователь {msg.author} удалил сообщение. "
-                    f"Содержание сообщения:\n{msg.content}"
-                    )
+        lang = load_server_language(msg)
+        config = find_server_config(msg)
+        if config["modlog"]["enabled"]:
+            ch = self.bot.get_channel(config["modlog"]["channel"])
+            await ch.send(
+                lang["misc"]["deleted_message"].replace(
+                    "$USER", msg.author.nick).replace("$MESSAGE", msg.content)
+                )
     
     @commands.Cog.listener()
     async def on_message_edit(self, msg_before, msg):
-        with open(self.find_server_config(msg), 'r') as raw_config:
-            config = yaml.load(raw_config, Loader)
-            if config["modlog"]["enabled"]:
-                ch = self.bot.get_channel(config["modlog"]["channel"])
-                await ch.send(
-                    f"Пользователь {msg.author} изменил сообщение. "
-                    f"Содержание сообщения до изменения:"
-                    f"\n{msg_before.content}\nСодержание сообщения "
-                    f"после изменения:\n{msg.content}"
-                    )
+        lang = load_server_language(msg)
+        config = find_server_config(msg)
+        if config["modlog"]["enabled"]:
+            ch = self.bot.get_channel(config["modlog"]["channel"])
+            await ch.send(
+                lang["misc"]["changed_message"].replace(
+                    "$USER", msg.author.nick).replace(
+                    "$OLD_MESSAGE", msg_before.content).replace("$NEW_MESSAGE",
+                                                                msg.content)
+                )
 
 
 def setup(bot):
