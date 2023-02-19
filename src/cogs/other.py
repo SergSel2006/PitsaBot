0910930579c705949f37e1264ce7685883df922b
SergSel2006 <seregasel44@gmail.com>
2023-02-20 00:07:19 +0700

    added counting joy

--- src/cogs/other.py
+++ src/cogs/other.py
@@ -42,6 +42,20 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 #      This program is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU General Public License as published by
 #      the Free Software Foundation, either version 3 of the License, or
@@ -115,6 +129,7 @@ async def counting(msg: discord.Message):
                         )
                     )
                 config["counting"]["number"] = 0
+                config["counting"]["last-counted-person"] = 0
             dump_server_config(msg, config)
 
 
